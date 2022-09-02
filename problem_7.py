975254e2a3204d039b85e0973d011f0f8be03a0d
Lahari <lxm13330@gitam.edu>
2022-09-01 22:54:01 -0500

    presentation video

--- problem_7.py
+++ problem_7.py
@@ -1,5 +1,5 @@
 # Use a tab escape sequence to get the following lines.
-#  Name Age Country City
-#  Asabeneh 250 Finland Helsinki
+#  Name       Age     Country    City
+#  Asabeneh    250     Finland    Helsinki
 
 print('Name\t\tAge\t\tCountry\t\tCity\nAsabeneh\t250\t\tFinland\t\tHelsinki')
